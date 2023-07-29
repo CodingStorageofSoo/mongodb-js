7252483ab5b471088e7048fa8746637fa909aba7
CodingStorageSoo <chlqudtn347@gmail.com>
2023-07-28 20:45:33 -0700

    7/28

--- server.js
+++ server.js
@@ -4,14 +4,33 @@ require("dotenv").config();
 // npm i express
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 // npm i mongoose
 const mongoose = require("mongoose");
+const User = require("./models/User");
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
+
+    app.post("/register", async (req, res) => {
+      const { userId, cdn } = req.body;
+      await new User({ userId: userId, cdn: cdn }).save();
+      res.json({ userId: userId, cdn: cdn });
+    });
+
+    app.post("/geturl", async (req, res) => {
+      const userId = req.body.userId;
+      const user = await User.findOne({ userId: userId });
+      if (user) {
+        res.json({ cdnURL: user.cdn });
+      } else {
+        res.status(404).json({ error: "User not found" });
+      }
+    });
+
     app.listen(process.env.PORT, () => {
       console.log(`Server listening on port ${process.env.PORT}`);
     });

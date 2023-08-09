94578a177be705b533673db934276c078fb756e2
CodingStorageSoo <chlqudtn347@gmail.com>
2023-08-09 09:37:49 -0700

    Update Data

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const { v4: uuidv4 } = require("uuid");
+
 // npm i mongoose
 const mongoose = require("mongoose");
 const User = require("./models/User");
+const Log = require("./models/Log");
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -28,6 +31,38 @@ mongoose
         res.json({ cdnURL: user.cdn });
       } else {
         res.status(404).json({ error: "User not found" });
+      }
+    });
+
+    app.post("/register-log", async (req, res) => {
+      const data = {
+        id: uuidv4(),
+        winner: req.body,
+        firePixel: "false",
+        click: "false",
+      };
+      await new Log(data).save();
+      res.json(data);
+    });
+
+    app.get("/update/:id", async (req, res) => {
+      try {
+        const updatedLog = await Log.findOneAndUpdate(
+          { id: req.params.id },
+          { $set: { firePixel: "true" } },
+          { new: true } // Return the updated document
+        );
+
+        if (updatedLog) {
+          console.log("Document updated successfully");
+          console.log(updatedLog);
+          res.json({ message: "Document updated successfully" });
+        } else {
+          res.status(404).json({ error: "Document not found" });
+        }
+      } catch (err) {
+        console.error("Error updating document:", err);
+        res.status(500).json({ error: "Internal server error" });
       }
     });
 
